31eafbbccca5ec289b78102d5e550b1089ec26ba
João Fernando Franco Tonsic <tonsic@gmail.com>
2018-12-15 21:50:08 -0200

    First commit, hello

--- src/main/kotlin/com/tonsic/changepackage/MainApp.kt
+++ src/main/kotlin/com/tonsic/changepackage/MainApp.kt
@@ -4,9 +4,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class MultiinvestmentGraphsApplication
+class MainApp
 
 fun main(args: Array<String>) {
-	runApplication<MultiinvestmentGraphsApplication>(*args)
+	runApplication<MainApp>(*args)
 }
 
